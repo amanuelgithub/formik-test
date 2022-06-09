fffa61630d54dbadd11292b8d53165bfde12a381
amanuelgithub <amanuelgirma070@gmail.com>
2022-06-09 21:54:38 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik } from 'formik'
+import { TextField } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className="App">
+    <Formik initialValues={{firstName:'Amanuel'}} onSubmit={(data) => {console.log(data)}}  >
+      {
+        ({values, handleChange, handleBlur, handleSubmit}) => (
+          <form onSubmit={handleSubmit}>
+            <TextField name="firstName" value={values.firstName} onChange={handleChange} onBlur={handleBlur} />
+          </form>
+        )
+      }
+    </Formik>
+  </div>;
+};
 
 export default App;
